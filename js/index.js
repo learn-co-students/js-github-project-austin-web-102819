eb5978e24405f5b651875d63639e2146a54fc5de
Farmwell <g.ferowich@gmail.com>
2020-03-18 11:46:56 -0500

    new user info displays on page - avatar, github url, name

--- js/index.js
+++ js/index.js
@@ -2,17 +2,37 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("we're up");
   const form = document.getElementById("github-form");
   const search = document.getElementById("search");
+  const githubUserList = document.getElementById("user-list");
 
-  form.addEventListener("submit", event => {
+  const createUser = json => {
+    console.log(json);
+    let user = document.createElement("li");
+    user.innerText = json.items[0].login;
+    githubUserList.appendChild(user);
+
+    let githubUrl = document.createElement("a");
+    githubUrl.innerHTML = "github url";
+    githubUrl.href = json.items[0].html_url;
+    githubUserList.appendChild(githubUrl);
+
+    let avatar = document.createElement("img");
+    avatar.src = json.items[0].avatar_url;
+    githubUserList.appendChild(avatar);
+  };
+
+  const LoadUserInfo = event => {
     event.preventDefault();
-    console.log(search.value);
-
     const API = `https://api.github.com/search/users?q=${search.value}`;
-    console.log(API);
+    console.log(search.value);
     fetch(API, {
       headers: { Accept: "application/vnd.github.v3+json" }
     })
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => createUser(data));
+  };
+
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+    LoadUserInfo(event);
   });
 });
